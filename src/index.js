3778ae7c1e70b61fda3d206e6653dbe4a16d35c8
Varvara Babkova <varvara.munday@gmail.com>
2019-08-06 14:40:29 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const form = document.querySelector("#create-task-form")
+    
+    form.addEventListener("submit", function(event){
+        event.preventDefault()
+        let task = event.target[0].value
+        const div = document.querySelector("#list")
+
+        const li = document.createElement("li")
+       	li.innerText = task
+
+        div.append(li)
+        event.target.reset()
+    })
+
 });
