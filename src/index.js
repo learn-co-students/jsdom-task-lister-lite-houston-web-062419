6d7fdc296b1ec0951ab5182cf513ca94ee8c909c
Sfimadi94 <sydeffect1994@gmail.com>
2019-08-06 15:32:03 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+    // console.log(event.target.children[1].value)
+    let comment = event.target.children[1].value
+
+    const div = document.querySelector("#list")
+
+    const p = document.createElement("p")
+    p.innerText = comment
+
+    div.append(p)
+
+    // event.target.reset()
+  })
+  
 });
